63f692b26786b5940622638ecd357d17d7cf0307
= <luke.ward@sequelgroup.co.uk>
2016-09-21 15:59:14 +0100

    Added mathead to article class

--- src/wp-content/themes/default/includes/class.article.php
+++ src/wp-content/themes/default/includes/class.article.php
@@ -10,9 +10,26 @@
             $this->_article->ID = $ID;
             $this->_article->url = get_the_permalink($ID);
             $this->_article->title = get_the_title($ID);
-            $this->_article->excerpt = get_field('excerpt') ? get_field('excerpt') : get_the_excerpt($ID);
+            $this->_article->excerpt = get_field('excerpt', $ID) ? get_field('excerpt', $ID) : get_the_excerpt($ID);
+
+            // Setup masthead
+            $masthead = get_field('masthead', $ID);
+
+            if (count($masthead)) {
+                $masthead = $masthead[0];
+                $this->_article->masthead = new stdClass();
+                $this->_article->masthead->type = $masthead['acf_fc_layout'];
+                switch($this->_article->masthead->type) {
+                    case 'image': $this->_article->masthead->url = $masthead['image']; break;
+                    case 'video': $this->_article->masthead->url = $masthead['video']; break;
+                    case 'html': $this->_article->masthead->html = $masthead['html']; break;
+                }
+            }
 
             $this->_article->thumbnail = get_field('thumbnail', $ID);
+            if (!$this->_article->thumbnail && $this->_article->masthead->type == 'image') {
+                $this->_article->thumbnail = $this->_article->masthead->url;
+            }
         }
 
         public function __get($name) {
