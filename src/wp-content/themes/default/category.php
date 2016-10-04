032dd758bd6184c6c938fb78d9a40489a3ab24cc
Rod Farry <rxfarry@googlemail.com>
2016-10-04 14:44:20 +0100

    fixed for rilling news

--- src/wp-content/themes/default/category.php
+++ src/wp-content/themes/default/category.php
@@ -2,6 +2,10 @@
     $site = new Site();
 
     $category = get_the_category();
-    $articles = $site->issue->getArticles(array('category' => $category[0]->term_id));
+    if($site->issueBased) {
+        $articles = $site->issue->getArticles(array('category' => $category[0]->term_id)); 
+    } else {
+        $articles = $site->getArticles(array('category'=>$category[0]->term_id));
+    }
 
     echo $twig->render('home.html', array('site'=> $site, 'articles' => $articles));
