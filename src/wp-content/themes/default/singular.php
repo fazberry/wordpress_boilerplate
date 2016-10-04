0006084a6d54d7a014eb0681fd4ef9e99dadc432
Rod Farry <rxfarry@googlemail.com>
2016-10-04 14:45:54 +0100

    fixed for rolling news

--- src/wp-content/themes/default/singular.php
+++ src/wp-content/themes/default/singular.php
@@ -2,6 +2,7 @@
     $site = new Site();
 
     $children = get_pages('child_of='.$post->ID);
+
     if(count($children) != 0 ) { 
 
         include('home.php');
@@ -11,7 +12,7 @@
         $article = $site->getArticle($post->ID);
 
         // Get sibling nav
-        $siblings = $article->getSiblings();
+        $siblings = $article->getSiblings($site->issueBased);
 
         echo $twig->render('article.html', array('site'=> $site, 'article' => $article, 'siblings' => $siblings));
 
