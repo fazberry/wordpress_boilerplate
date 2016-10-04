bfeea50b59a14262bb33462a0ff0b99ca437a540
Rod Farry <rxfarry@googlemail.com>
2016-10-04 14:44:46 +0100

    fixed for rolling news

--- src/wp-content/themes/default/home.php
+++ src/wp-content/themes/default/home.php
@@ -1,6 +1,11 @@
 <?php
     $site = new Site();
-    $articles = $site->issue->getArticles(array('posts_per_page' => 6));
+
+    if($site->issueBased) {
+        $articles = $site->issue->getArticles(array('posts_per_page' => 6));
+    } else {
+        $articles = $site-> getArticles();
+    }
 
     echo $twig->render('home.html', array('site'=> $site, 'articles' => $articles));
 
